b76157c9dd798246d0dfd654c7c876ccf10bb828
Pedro Castro <falecompedroac@gmail.com>
2024-03-08 13:47:34 +0000

    finished 3rd week sheet

--- da2324_p03_student/TP3/ex2.cpp
+++ da2324_p03_student/TP3/ex2.cpp
@@ -4,9 +4,41 @@
 #include "../data_structures/Graph.h"
 #include "MSTTestAux.h"
 
-template <class T>
-std::vector<Vertex<T> *> prim(Graph<T> * g) {
-    // TODO
+template<class T>
+std::vector<Vertex<T> *> prim(Graph<T> *g) {
+    if (g->getVertexSet().empty())
+        return g->getVertexSet();
+
+    for (Vertex<T> *v: g->getVertexSet()) {
+        v->setDist(INF);
+        v->setPath(nullptr);
+        v->setVisited(false);
+    }
+
+    Vertex<T> *r = g->getVertexSet().front();
+    r->setDist(0.0);
+
+    MutablePriorityQueue<Vertex<T>> q;
+    q.insert(r);
+
+    while (!q.empty()) {
+        Vertex<T> *u = q.extractMin();
+        u->setVisited(true);
+
+        for (Edge<T> *e: u->getAdj()) {
+            Vertex<T> *v = e->getDest();
+
+            if (v->getDist() == INF)
+                q.insert(v);
+
+            if (!v->isVisited() && e->getWeight() < v->getDist()) {
+                v->setPath(e);
+                v->setDist(e->getWeight());
+                q.decreaseKey(v);
+            }
+        }
+    }
+
     return g->getVertexSet();
 }
 
@@ -18,9 +50,9 @@ TEST(TP3_Ex2, test_prim) {
     std::vector<Vertex<int> *> res = prim(&graph);
 
     std::stringstream ss;
-    for(const auto v : res) {
+    for (const auto v: res) {
         ss << v->getInfo() << "<-";
-        if ( v->getPath() != nullptr ) {
+        if (v->getPath() != nullptr) {
             ss << v->getPath()->getOrig()->getInfo();
         }
         ss << "|";
--- da2324_p03_student/TP3/ex3.cpp
+++ da2324_p03_student/TP3/ex3.cpp
@@ -4,14 +4,62 @@
 #include "MSTTestAux.h"
 #include "../data_structures/UFDS.h"
 
-template <class T>
+template<class T>
 void dfsKruskalPath(Vertex<T> *v) {
-    // TODO
+    v->setVisited(true);
+
+    for (Edge<T> *e: v->getAdj()) {
+        Vertex<T> *w = e->getDest();
+        if (e->isSelected() && !w->isVisited()) {
+            w->setPath(e);
+            dfsKruskalPath(w);
+        }
+    }
 }
 
-template <class T>
+template<class T>
 std::vector<Vertex<T> *> kruskal(Graph<T> *g) {
-    // TODO
+    if (g->getVertexSet().empty())
+        return g->getVertexSet();
+
+    vector<Edge<T> *> edges;
+    UFDS ufds(g->getNumVertex());
+    int id = 0;
+    unsigned int edgesCount = 0;
+
+    for (Vertex<T> *v: g->getVertexSet()) {
+        v->setVisited(false);
+        v->setPath(nullptr);
+        v->setInfo(id++);
+
+        for (Edge<T> *e: v->getAdj()) {
+            e->setSelected(false);
+            if (e->getOrig()->getInfo() < e->getDest()->getInfo())
+                edges.push_back(e);
+        }
+    }
+
+    sort(edges.begin(), edges.end(),
+         [](const Edge<T> *e1, const Edge<T> *e2) -> bool { return e1->getWeight() < e2->getWeight(); });
+
+    for (Edge<T> *e: edges) {
+        Vertex<T> *u = e->getOrig();
+        Vertex<T> *v = e->getDest();
+
+        if (!ufds.isSameSet(u->getInfo(), v->getInfo())) {
+            ufds.linkSets(u->getInfo(), v->getInfo());
+
+            e->setSelected(true);
+            e->getReverse()->setSelected(true);
+
+            if (++edgesCount == g->getNumVertex() - 1)
+                break;
+        }
+    }
+
+    g->getVertexSet()[0]->setPath(nullptr);
+    dfsKruskalPath(g->getVertexSet()[0]);
+
     return g->getVertexSet();
 }
 
@@ -23,9 +71,9 @@ TEST(TP3_Ex3, test_kruskal) {
     std::vector<Vertex<int> *> res = kruskal(&graph);
 
     std::stringstream ss;
-    for(const auto v : res) {
+    for (const auto v: res) {
         ss << v->getInfo() << "<-";
-        if ( v->getPath() != nullptr ) {
+        if (v->getPath() != nullptr) {
             ss << v->getPath()->getOrig()->getInfo();
         }
         ss << "|";
